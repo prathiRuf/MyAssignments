eb63ae67b6a485008ec52a2a38f104c41831058f
PrathibhaA <90465671+prathiRuf@users.noreply.github.com>
2023-09-12 17:16:16 +0530

    Add files via upload

---
+++ assignments/NegativeNumber.java
@@ -0,0 +1,25 @@
+package week1.day1.assignments;
+
+public class NegativeNumber {
+
+	public static void main(String[] args) {
+		/*Assignment:3
+Problem statement: Convert a negative number to positive number
+-----------------------------------------------
+Pseudocode: 
+1. Initialize an integer with a negative number like, int number = -40;2. Check if the number is a negative number Hint : any number that is lesser than zero is a negative number3. If so, convert the number to a positive numer
+
+4. Print as below "The number -40 is converted to 40"*/
+		
+		int i=45;
+		int j=-1;
+		if(i<0) {
+			j=i*j;
+			System.out.println("The given number "+i+" is converted to positive "+j);
+		}
+		else
+			System.out.println("Enter negative number");
+
+	}
+
+}
---
+++ assignments/PositiveOrNegative.java
@@ -0,0 +1,25 @@
+package week1.day1.assignments;
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		/*Assignment:4
+
+Problem statement: Check if the number is positive or negative
+--------------------------------------------------------------
+-Pseudocode:---------- 
+1. Initialize a number, say, int number= 35; 
+2. If a number is positive, print "The number is positive"
+ If a number is negative, print "The number is negative" 
+If it nether negative nor positive, print as "The number is neither positive nor negative"*/
+		
+		int i=0;
+		if(i>0)
+			System.out.println("The number is positive");
+		else if(i<0)
+			System.out.println("The number is negative");
+		else
+			System.out.println("The number is 0");
+
+	}
+
+}
